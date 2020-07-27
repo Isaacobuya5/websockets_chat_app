6dc4a2a3318e9fdf4755c94c7bff8e42bb7537f4
Isaac Obuya <Szack4477@gmail.com>
2020-07-27 06:55:07 +0300

    refactor code to create server outside express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const path = require('path');
+
+const http = require('http');
 const express = require('express');
 
 const app = express();
+const server = http.createServer(app);
 
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+server.listen(port, () => console.log(`Server listening on port ${port}`));
