9b18a7ca8e3db2ff1913b4b7ef6b3cc1e0f6c738
Isaac Obuya <Szack4477@gmail.com>
2020-07-28 06:34:07 +0300

    sending and receiving events using socketio

--- src/index.js
+++ src/index.js
@@ -2,13 +2,31 @@ const path = require('path');
 
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+// socketio expects it to be called with raw http server
+const io = socketio(server);
 
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
 const port = process.env.PORT || 3000;
+
+let count = 0;
+
+io.on('connection', (socket) => { 
+    console.log('New web socket connection') 
+    // sending an event to the client
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++;
+        // socket.emit('countUpdated', count)
+        // emitting to every client connection available
+        io.emit('countUpdated', count);
+    })
+});
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
