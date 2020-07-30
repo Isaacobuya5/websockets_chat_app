e740c8677cbb152e28d5094cfaf7637fea342ddf
Isaac Obuya <Szack4477@gmail.com>
2020-07-30 07:33:07 +0300

    allow users join chat rooms and only send messages to those rooms

--- src/index.js
+++ src/index.js
@@ -59,9 +59,16 @@ let message = "Welcome!";
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+    // socket.emit('message', generateMessage('Welcome!'))
+    // socket.broadcast.emit('message', generateMessage('A new user has joined!'))
 
+    socket.on('join', ({ username, room}) => {
+        // this allows us to join a room
+        // by joining a room, we will emit events only to that particular room
+        socket.join(room)
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined`))
+    })
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
 
