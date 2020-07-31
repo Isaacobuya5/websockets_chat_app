f4172ca19204ae1a777a29ce5ee0b2d415fa97c2
Isaac Obuya <Szack4477@gmail.com>
2020-07-31 07:00:22 +0300

    track users in rooms

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -62,12 +63,24 @@ io.on('connection', (socket) => {
     // socket.emit('message', generateMessage('Welcome!'))
     // socket.broadcast.emit('message', generateMessage('A new user has joined!'))
 
-    socket.on('join', ({ username, room}) => {
+    socket.on('join', (options, callback) => {
+
+        const { error, user } = addUser({
+            id: socket.id,
+            ...options
+        });
+
+        if (error) {
+            return callback(error);
+        }
         // this allows us to join a room
         // by joining a room, we will emit events only to that particular room
-        socket.join(room)
+        socket.join(user.room)
         socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined`))
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined`))
+
+        // user succesfully joined
+        callback();
     })
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
@@ -87,7 +100,12 @@ io.on('connection', (socket) => {
 
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'))
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+        }
+        
     })
 })
 
